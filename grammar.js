236a7363438d31c5421ce4d326af153117b4ab99
Ryan Halliwell <git@rayliwell.com>
2024-02-27 01:41:08 +0000

    Add grammar for parsing rstml vs non-rstml macros
    This determines whether a macro invocation is rstml related or not
    based on its identifier. When a macro is determined to be rstml
    related, it is parsed in a different manner.

--- grammar.js
+++ grammar.js
@@ -14,5 +14,33 @@ module.exports = grammar({
 
   rules: {
     ...rustGrammar.rules,
+
+    macro_invocation: $ =>
+      choice($._non_rstml_macro_invocation, $._rstml_macro_invocation),
+
+    _non_rstml_macro_invocation: $ =>
+      seq(
+        field(
+          'macro',
+          choice($.scoped_identifier, $.identifier, $._reserved_identifier),
+        ),
+        '!',
+        alias($.delim_token_tree, $.token_tree),
+      ),
+
+    _rstml_macro_invocation: $ =>
+      seq(
+        field('macro', alias($.rstml_macro_identifier, $.identifier)),
+        '!',
+        $._delim_nodes,
+      ),
+
+    // TODO: Add rstml identifiers or better way of deciding macros.
+    rstml_macro_identifier: _ => choice('view'),
+
+    _delim_nodes: $ => seq('{', $.nodes, '}'),
+
+    // TODO: Implement nodes grammar.
+    nodes: _ => repeat1('!'),
   },
 })
