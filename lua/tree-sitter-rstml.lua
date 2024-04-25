c3faa15af7e9a20b1d7be828d7bcf1d9d40a26b8
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-04-25 20:39:30 +0200

    fix: nvim-treesitter main branch support after breaking change

--- lua/tree-sitter-rstml.lua
+++ lua/tree-sitter-rstml.lua
@@ -1,19 +1,30 @@
 local M = {}
 
 function M.init()
-	local parsers = require("nvim-treesitter.parsers")
-	-- Support nvim-treesitter versions before and after 1.10.
-	local configs = parsers.configs and parsers.configs or parsers.get_parser_configs()
-
-	configs.rust_with_rstml = {
-		install_info = {
-			url = "https://github.com/rayliwell/tree-sitter-rstml",
-			branch = "main",
-			files = { "src/parser.c", "src/scanner.c" },
-			location = "rust_with_rstml",
-		},
-		filetype = "rust",
+	local install_info = {
+		url = "https://github.com/rayliwell/tree-sitter-rstml",
+		branch = "main",
+		files = { "src/parser.c", "src/scanner.c" },
+		location = "rust_with_rstml",
 	}
+
+	local ok, parsers = pcall(require("nvim-treesitter.parsers").get_parser_configs)
+	if ok then
+		parsers.rust_with_rstml = {
+			install_info = install_info,
+			filetype = "rust",
+		}
+	else
+		-- Support nvim-treesitter versions before and after 1.10.
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "TSUpdate",
+			callback = function()
+				require("nvim-treesitter.parsers").rust_with_rstml = {
+					install_info = install_info,
+				}
+			end,
+		})
+	end
 end
 
 function M.setup()
