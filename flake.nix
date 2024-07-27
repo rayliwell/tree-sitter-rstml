4c9b1f974e4938650c0dd54d7aaf099879a07b56
Ryan Halliwell <git@rayliwell.com>
2024-07-28 00:57:48 +0100

    flake: migrate from `flake-utils`

--- flake.nix
+++ flake.nix
@@ -3,87 +3,86 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      rec {
-        devShells = {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              nodejs
-              rustc
-              cargo
-              gcc
-              gnumake
-              (tree-sitter.override { webUISupport = true; })
-              python3
-              emscripten
-            ];
-          };
-        };
-
-        packages = rec {
-          tree-sitter-grammars =
-            let
-              buildGrammar =
-                grammarName:
-                pkgs.tree-sitter.buildGrammar {
-                  version = "1.2.0";
-                  src = ./.;
-                  generate = false;
-                  location = grammarName;
-                  language = grammarName;
-                };
-
-            in
-            {
-              rstml = buildGrammar "rstml";
-              rust_with_rstml = buildGrammar "rust_with_rstml";
-            };
-
-          nvim-treesitter-grammar = tree-sitter-grammars.rust_with_rstml.overrideAttrs (
-            final: prev: {
-              postInstall = ''
-                rm -r $out/queries
-                mkdir $out/queries
-                cp -r ../queries/rust_with_rstml/. $out/queries/
-              '';
-            }
-          );
-        };
-
-        nixvimModule =
-          { config, ... }:
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "aarch64-darwin"
+          "aarch64-linux"
+          "i686-linux"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    rec {
+      packages = forAllSystems (pkgs: rec {
+        tree-sitter-grammars =
+          let
+            buildGrammar =
+              grammarName:
+              pkgs.tree-sitter.buildGrammar {
+                version = "1.2.0";
+                src = ./.;
+                generate = false;
+                location = grammarName;
+                language = grammarName;
+              };
+          in
           {
-            plugins = {
-              treesitter = {
-                grammarPackages = config.plugins.treesitter.package.passthru.allGrammars ++ [
-                  packages.nvim-treesitter-grammar
-                ];
+            rstml = buildGrammar "rstml";
+            rust_with_rstml = buildGrammar "rust_with_rstml";
+          };
 
-                languageRegister = {
-                  rust_with_rstml = [ "rust" ];
-                };
+        nvim-treesitter-grammar = tree-sitter-grammars.rust_with_rstml.overrideAttrs (
+          final: prev: {
+            postInstall = ''
+              rm -r $out/queries
+              mkdir $out/queries
+              cp -r ../queries/rust_with_rstml/. $out/queries/
+            '';
+          }
+        );
+      });
+
+      nixvimModule =
+        { config, pkgs, ... }:
+        {
+          plugins = {
+            treesitter = {
+              grammarPackages = config.plugins.treesitter.package.passthru.allGrammars ++ [
+                packages.${pkgs.system}.nvim-treesitter-grammar
+              ];
+
+              languageRegister = {
+                rust_with_rstml = [ "rust" ];
               };
             };
           };
-
-        overlays = {
-          default = final: prev: {
-            tree-sitter-grammars = prev.tree-sitter-grammars // packages.tree-sitter-grammars;
-          };
         };
-      }
-    );
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            rustc
+            cargo
+            gcc
+            gnumake
+            (tree-sitter.override { webUISupport = true; })
+            python3
+            emscripten
+          ];
+        };
+      });
+
+      overlays = {
+        default = final: prev: {
+          tree-sitter-grammars = prev.tree-sitter-grammars // packages.tree-sitter-grammars;
+        };
+      };
+    };
 }
