f5432a5599fd9808a4a4e5bf255efe9a00be51ef
Ryan Halliwell <git@rayliwell.com>
2025-02-18 15:41:40 +0000

    fix: override highlight priority of semantic highlights

--- base_queries/highlights.scm
+++ base_queries/highlights.scm
@@ -1,33 +1,33 @@
 ((doctype_node) @constant
-  (#set! "priority" 101))
+  (#set! "priority" 150))
 
 (doctype_node
   [
     "<!"
     ">"
   ] @tag.delimiter
-  (#set! "priority" 101))
+  (#set! "priority" 150))
 
 (open_tag
   [
     "<"
     ">"
   ] @tag.delimiter
-  (#set! "priority" 101))
+  (#set! "priority" 150))
 
 (close_tag
   [
     "</"
     ">"
   ] @tag.delimiter
-  (#set! "priority" 101))
+  (#set! "priority" 150))
 
 (self_closing_element_node
   [
     "<"
     "/>"
   ] @tag.delimiter
-  (#set! "priority" 101))
+  (#set! "priority" 150))
 
 (node_identifier
   [
@@ -35,26 +35,26 @@
     ":"
     "::"
   ] @punctuation.delimiter
-  (#set! "priority" 103))
+  (#set! "priority" 152))
 
 (open_tag
   name: (node_identifier) @tag
-  (#set! "priority" 102))
+  (#set! "priority" 151))
 
 (close_tag
   name: (node_identifier) @tag
-  (#set! "priority" 102))
+  (#set! "priority" 151))
 
 (self_closing_element_node
   name: (node_identifier) @tag
-  (#set! "priority" 102))
+  (#set! "priority" 151))
 
 ((node_identifier) @tag.attribute
-  (#set! "priority" 101))
+  (#set! "priority" 150))
 
 (node_attribute
   name: (node_identifier) @tag.attribute
-  (#set! "priority" 101))
+  (#set! "priority" 150))
 
 (generic_identifier
   [
@@ -64,14 +64,14 @@
     ] @punctuation.delimiter
     (node_identifier) @tag
   ]
-  (#set! "priority" 101))
+  (#set! "priority" 150))
 
 ((text_node) @variable
-  (#set! "priority" 101))
+  (#set! "priority" 150))
 
 (comment_node
   [
     "<!--"
     "-->"
   ] @comment
-  (#set! "priority" 101))
+  (#set! "priority" 150))
